693e863c27a1fbcc1850b770dc2d80dc940bc798
Eric-Derencio <eric.derencio@simples.software>
2025-09-23 09:10:30 -0300

    feat: add layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    loadComponent: ()=> import('./shared/components/layout/layout').then(m=>m.Layout),
+    children:[{
+      path:'',
+      redirectTo:'projecoes',
+      pathMatch:'full'
+    },
+    {
+      path: 'projecoes',
+      loadComponent: () => import('./domain/projections/projection/projection').then(m => m.Projection)
+    },
+    {
+      path: 'receita',
+      loadComponent: () => import('./domain/revenues/revenue/revenue').then(m => m.Revenue)
+    },
+    {
+      path: 'contas',
+      loadComponent: () => import('./domain/bills/bill/bill').then(m => m.Bill)
+    },
+  ]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
